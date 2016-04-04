c98ad2fd4f037c5b4196097a180ac482933a62ee
Roman Messer <rms@gotocloud.net>
2016-04-04 13:51:54 +0200

    Add Padrino base
    Components are ministest, DataMapper and Liquid.

---
+++ lib/identity_map_middleware.rb
@@ -0,0 +1,12 @@
+class IdentityMap
+  def initialize(app, name = :default)
+    @app = app
+    @name = name.to_sym
+  end
+
+  def call(env)
+    ::DataMapper.repository(@name) do
+      @app.call(env)
+    end
+  end
+end
---
+++ test/test_config.rb
@@ -0,0 +1,21 @@
+RACK_ENV = 'test' unless defined?(RACK_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+Dir[File.expand_path(File.dirname(__FILE__) + "/../app/helpers/**/*.rb")].each(&method(:require))
+
+class MiniTest::Spec
+  include Rack::Test::Methods
+
+  # You can use this method to custom specify a Rack app
+  # you want rack-test to invoke:
+  #
+  #   app GetVolunteers::App
+  #   app GetVolunteers::App.tap { |a| }
+  #   app(GetVolunteers::App) do
+  #     set :foo, :bar
+  #   end
+  #
+  def app(app = nil, &blk)
+    @app ||= block_given? ? app.instance_eval(&blk) : app
+    @app ||= Padrino.application
+  end
+end
