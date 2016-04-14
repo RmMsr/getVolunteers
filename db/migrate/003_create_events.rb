0eae848e9fadc67e630b57b62c49192921a1aec3
Roman Messer <rms@gotocloud.net>
2016-04-14 15:02:21 +0200

    Fix name error in Events migration

--- db/migrate/003_create_events.rb
+++ db/migrate/003_create_events.rb
@@ -4,7 +4,7 @@ migration 3, :create_events do
       column :id, Integer, :serial => true
       column :start, DataMapper::Property::DateTime
       column :finish, DataMapper::Property::DateTime
-      column :event, DataMapper::Property::Slug
+      column :series, DataMapper::Property::Slug
       column :volunteers_goal, DataMapper::Property::Integer
       column :volunteers_current, DataMapper::Property::Integer
     end
